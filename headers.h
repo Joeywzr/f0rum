2e2ed6deae8876e88e6b8f4f0d7d12cf68e6c9e1
Memo <JoeyWang98@outlook.com>
2018-12-15 11:52:00 +0800

    加注释

--- headers.h
+++ headers.h
@@ -32,16 +32,16 @@ extern QVector<user_variable> all_users;//所有用户
 class user_variable
 {
 public:
-    int id;
-    QString username;
-    QString password;
-    QString level;
-    int responsible_plate;
-    int user_error = 0;
-
+    int id;//ID
+    QString username;//用户名
+    QString password;//密码
+    QString level;//等级
+    int responsible_plate;//版主负责板块，管理员和普通用户为0
+    int user_error = 0;//读入用户信息的错误个数
+    //重载<<运算符
     friend QTextStream& operator << (QTextStream &fout, user_variable &users)
     {
-        for(int i = 0;i < all_users.size();i++)
+        for(int i = 0;i < all_users.size();i++)//将用户信息存入文件，每个用户的各种信息以空格相隔，不同用户用回车相隔
         {
             users = all_users[i];
             fout << QString::number(users.id) << " ";
@@ -52,33 +52,33 @@ public:
         }
         return fout;
     }
+    //重载>>运算符
     friend QTextStream& operator >> (QTextStream &fin, user_variable &users)
     {
-        QString line;
-        QList<QString> strlist;
-        bool flag = true;
+        QString line;//每行信息
+        QList<QString> strlist;//存取每个用户的各种信息
+        bool flag = true;//用来判断用户信息是否出错
         while(!fin.atEnd())
         {
-            line = fin.readLine();
-            strlist = line.split(" ");
+            line = fin.readLine();//按行读入
+            strlist = line.split(" ");//把读入的行按空格分隔
 
-            if(strlist.size() != 5)
+            if(strlist.size() != 5)//如果每个用户信息不足5项，则报错，跳过读入该用户信息
                 users.user_error++;
             else
             {
                 users.id = strlist[0].toInt();
                 if(users.id == 0)
                     flag = false;
-
                 users.username = strlist[1];
                 users.password = strlist[2];
-
                 users.level = strlist[3];
+                //用户等级信息错误
                 if(users.level != "administrator" && users.level != "ordinary" && users.level != "moderator")
                     flag = false;
 
                 users.responsible_plate = strlist[4].toInt();
-                if(users.responsible_plate < 0 || users.responsible_plate > 5)
+                if(users.responsible_plate < 0 || users.responsible_plate > 5)//版主负责板块错误
                     flag = false;
                 if(flag)
                     all_users.push_back(users);
--- post.cpp
+++ post.cpp
@@ -5,3 +5,114 @@ Post::Post()
 
 
 }
+//重载Post类<<运算符
+friend QTextStream& Post::operator << (QTextStream &fout, Post posts)
+{
+    bool is_first = true;//是否是每个帖子的的开头
+    for(int i = 1;i <= 5;i++)
+    {
+        Category state;
+        QString s;
+        switch (i) {
+        case 1:
+            state = game;s = "game";break;
+        case 2:
+            state = movie;s = "movie";break;
+        case 3:
+            state = comic;s = "comic";break;
+        case 4:
+            state = music;s = "music";break;
+        case 5:
+            state = sports;s = "sports";break;
+        default:break;
+        }
+        QVector<Post> p = all_post[state];
+        for(int j = 0;j < p.size();j++)
+        {
+            posts = p[j];
+            if(!is_first)
+                fout << " ### ";   //把每个帖子用" ### "分隔
+            fout << s << " $$$ ";  //把每个帖子的内部信息用" $$$ "分隔
+            fout << QString::number(posts.id) << " $$$ ";
+            fout << posts.poster_name << " $$$ ";
+            fout << posts.time << " $$$ ";
+            fout << QString::number(posts.comment_num) << " $$$ ";
+            fout << posts.title << " $$$ ";
+            fout << posts.content;
+            if(posts.comment.size() != 0)
+                fout << " $$$ ";
+            for(int k = 0;k < posts.comment.size();k++)
+            {
+                fout << posts.comment[k].username << " $$$ ";
+                fout << posts.comment[k].time << " $$$ ";
+                fout << posts.comment[k].content;
+                if(k != posts.comment.size() - 1)
+                    fout << " $$$ ";
+            }
+            is_first = false;
+        }
+    }
+    return fout;
+}
+//重载Post类>>运算符
+friend QTextStream& operator >> (QTextStream &fin, Post &posts)
+{
+    QString str;
+    str = fin.readAll();
+    QList<QString> list1, list2;
+    QList<QString>::iterator i;
+    bool flag = true;
+    list1 = str.split(" ### ");//按" ### "分隔读取各个帖子
+
+    for(i = list1.begin();i != list1.end();i++)
+    {
+        list2 = (*i).split(" $$$ ");//按" $$$ "分隔读取帖子内部信息
+        if(list2.size() != 7 + list2[4].toInt()*3)//如果帖子内部信息数量不对应
+            posts.post_error++;
+        else
+        {
+            posts.state = list2[0];
+            if(posts.state != "game" &&posts.state != "movie" &&posts.state != "comic" &&posts.state != "music" && posts.state != "sports")
+                flag = false;
+            posts.id = list2[1].toInt();
+            posts.poster_name = list2[2];
+            posts.time = list2[3];
+            posts.comment_num = list2[4].toInt();
+            posts.title = list2[5];
+            posts.content = list2[6];
+            //读取评论信息
+            for(int j = 7;j < list2.size();j++)
+            {
+                if(j % 3 == 1)
+                    com.username = list2[j];
+                else if(j % 3 == 2)
+                    com.time = list2[j];
+                else if(j % 3 == 0)
+                {
+                    com.content = list2[j];
+                    posts.comment.push_back(com);
+                }
+            }
+            if(flag)//如果没错误就读入该帖子信息
+            {
+                if(posts.state == "game")
+                    all_post[game].push_back(posts);
+                else if(posts.state == "movie")
+                    all_post[movie].push_back(posts);
+                else if(posts.state == "comic")
+                    all_post[comic].push_back(posts);
+                else if(posts.state == "music")
+                    all_post[music].push_back(posts);
+                else if(posts.state == "sports")
+                    all_post[sports].push_back(posts);
+            }
+            else//若帖子有错则跳过该帖子不读入
+            {
+                posts.post_error++;
+                flag = true;
+            }
+            posts.comment.clear();
+        }
+    }
+    return fin;
+}
